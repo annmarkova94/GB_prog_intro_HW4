43181113c58055f84a2812fa493e8701940bac37
annmark94 <annmarkova94@gmail.com>
2022-07-07 21:44:44 +0300

    Initial commit, added the folder Task27 v1

---
+++ Task27_digitsSum_v1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Take a number and return sum of its digits (I use only natural numbers)
+// 1st option using a string
+// Not sure that using item.ToString() is the best way
+
+DigitsSum();
+
+void DigitsSum()
+{
+    string strNumber = Convert.ToString(EnterNaturalNumber());
+    int digitsSum = 0;
+    foreach (var item in strNumber)
+    {
+        digitsSum += int.Parse(item.ToString());
+    }
+    Console.WriteLine($"The number is {strNumber}, sum of its digits equals {digitsSum}");
+}
+
+int EnterNaturalNumber()
+{
+    int natural_number;
+    Console.Write("Enter a natural number: ");
+    while (!int.TryParse(Console.ReadLine(), out natural_number)
+           || natural_number < 1)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("This is not a natural number, try again: ");
+        Console.ResetColor();
+    }
+    return natural_number;
+}
