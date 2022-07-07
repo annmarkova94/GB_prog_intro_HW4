6d783a81aac5fde72fbeddc622fb3dfa13f2c4d4
annmark94 <annmarkova94@gmail.com>
2022-07-07 21:45:18 +0300

    Initial commit, added the folder Task27 v2

---
+++ Task27_digitsSum_v2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Take a number and return sum of its digits (I use only natural numbers)
+// 2st option using an integer
+
+DigitsSum();
+
+void DigitsSum()
+{
+    int ourNumber = EnterNaturalNumber();
+    int ourNumberForHelp = ourNumber;
+    int digitsSum = 0;
+    while (ourNumberForHelp > 0)
+    { 
+        digitsSum += ourNumberForHelp % 10;
+        ourNumberForHelp /= 10;
+    }
+    Console.WriteLine($"The number is {ourNumber}, sum of its digits equals {digitsSum}");
+}
+
+int EnterNaturalNumber()
+{
+    int natural_number;
+    Console.Write("Enter a natural number: ");
+    while (!int.TryParse(Console.ReadLine(), out natural_number)
+           || natural_number < 1)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("This is not a natural number, try again: ");
+        Console.ResetColor();
+    }
+    return natural_number;
+}
